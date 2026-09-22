1d7b58137cd3004e18227ffb82a814d75cb4a472
cincobenta-prog <cincobenta@gmail.com>
2026-09-22 14:17:32 -0400

    feat(auth): add Dorrence password security gate and vercel deployment configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { SitePasswordGate } from './components/auth/SitePasswordGate';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SitePasswordGate>
+      <App />
+    </SitePasswordGate>
   </React.StrictMode>,
 );
