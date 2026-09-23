6cf15a010945f8c5aa5e800c20068703ac1719bc
cincobenta-prog <cincobenta@gmail.com>
2026-09-23 11:43:43 -0400

    feat: remove password gate from site

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { SitePasswordGate } from './components/auth/SitePasswordGate';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SitePasswordGate>
-      <App />
-    </SitePasswordGate>
+    <App />
   </React.StrictMode>,
 );
+
